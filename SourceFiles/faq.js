e1daf91feaccfaadedf2a91a241b6debd65c380e
Hannah Zahler <124529272+HannahZ48@users.noreply.github.com>
2024-10-23 03:07:39 -0400

    Update faq.js

--- SourceFiles/faq.js
+++ SourceFiles/faq.js
@@ -2,12 +2,12 @@ const toggle = document.getElementById('nav-toggle')
 const nav = document.getElementById('nav')
 const toggles = document.querySelectorAll('.faq-toggle')
 
-toggle.addEventListener('click', function(){
-    nav.classList.toggle('active')
-})
-
 toggles.forEach(toggle => {
     toggle.addEventListener('click', () => {
         toggle.parentNode.classList.toggle('active')
     })
+})
+
+toggle.addEventListener('click', function(){
+    nav.classList.toggle('active')
 })
