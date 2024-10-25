82776a747ebfd194e2872c8d408a038c56be64ec
Toufiq Hossian <146899761+toufiqhossian@users.noreply.github.com>
2024-10-24 22:29:45 -0400

    adding back the customer review slideshow on the home page and the customer rating popup on the sale page.

--- SourceFiles/HomeScript.js
+++ SourceFiles/HomeScript.js
@@ -4,3 +4,75 @@ const nav = document.getElementById('nav')
 toggle.addEventListener('click', function(){
     nav.classList.toggle('active')
 })
+
+const testimonialContainer = document.querySelector('.testimonial-container');
+
+const testimonial = document.querySelector('.testimonial');
+const userImage = document.querySelector('.user-image');
+const username = document.querySelector('.username');
+
+const testimonials = [
+  {
+    name: 'Miyah Myles',
+    photo:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6',
+    text:
+      "CodingUniverse has transformed my career. The courses are comprehensive and perfectly catered to my needs.",
+  },
+  {
+    name: 'June Cha',
+    photo: 'https://randomuser.me/api/portraits/women/44.jpg',
+    text:
+      'I\'m amazed at the quality of content on CodingUniverse. It\'s like having a personal mentor guiding you through complex topics.',
+  },
+  {
+    name: 'Iida Niskanen',
+    photo: 'https://randomuser.me/api/portraits/women/68.jpg',
+    text:
+      'The flexibility of CodingUniverse\'s courses allowed me to learn at my own pace and make a career change I\'d always dreamed of.',
+  },
+  {
+    name: 'Renee Sims',
+    photo: 'https://randomuser.me/api/portraits/women/65.jpg',
+    text:
+      'CodingUniverse\'s courses are a game-changer for my team. We\'ve upskilled and improved our productivity dramatically.',
+  },
+  {
+    name: 'Jonathan Nunfiez',
+    photo: 'https://randomuser.me/api/portraits/men/43.jpg',
+    text:
+      'I\'m a lone learner, and CodingUniverse has been my go-to for personal development. The courses are top-notch.',
+  },
+  {
+    name: 'Sasha Ho',
+    photo:
+      'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?h=350&auto=compress&cs=tinysrgb',
+    text:
+      'Kudos to CodingUniverse for making high-quality education accessible. These courses have opened up new horizons for me.',
+  },
+  {
+    name: 'Veeti Seppanen',
+    photo: 'https://randomuser.me/api/portraits/men/97.jpg',
+    text:
+      'CodingUniverse is my secret weapon for staying competitive in the tech industry. The courses keep me ahead of the curve.',
+  },
+];
+
+let idx = 1;
+
+function updateTestimonial() {
+  const { name, photo, text } = testimonials[idx];
+
+  testimonial.innerHTML = text;
+  userImage.src = photo;
+  username.innerHTML = name;
+
+  idx++;
+
+  if (idx > testimonials.length - 1) {
+    idx = 0;
+  }
+}
+setInterval(updateTestimonial, 10000);
+
+  
